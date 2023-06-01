d42b7000b7c810a911004663f6d6e78b99ad463d
Tim D <124634979+TimothyDoren@users.noreply.github.com>
2023-06-01 15:22:33 -0400

    Add project files.

---
+++ Valid Parens/Program.cs
@@ -0,0 +1,13 @@
+﻿using Valid_Parens;
+
+Solution sol = new Solution();
+string[] s = { "((({{}])))", "(((())))", "(([{}]))", "(({{[[]]})))", "{[]}" };
+//false
+//true
+//true
+//false
+//true 
+foreach(string p in s)
+{
+    Console.WriteLine(sol.IsValid(p));
+}
---
+++ Valid Parens/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valid_Parens
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            bool isValid = false;
+            Stack<char> parens = new Stack<char>();
+            foreach (char p in s)
+            {
+                if (p == '(' || p == '[' || p == '{')
+                {
+                    parens.Push(p);
+                }
+                else
+                {
+                    if (parens.Count == 0)
+                    {
+                        return false;
+                    }
+                    char first = parens.Pop();
+                    if (p == ')' && first != '(' ||
+                    p == ']' && first != '[' ||
+                    p == '}' && first != '{')
+                    {
+                        return false;
+                    }
+                }
+            }
+            return parens.Count() == 0;
+        }
+    }
+}
